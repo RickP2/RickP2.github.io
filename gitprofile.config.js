6e63db373ec6fe27899a0e049d802b135b96a712
Ricky Price <64097508+RickP2@users.noreply.github.com>
2023-04-27 16:20:26 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -11,7 +11,7 @@ const config = {
     },
   },
   social: {
-    linkedin: 'https://www.linkedin.com/in/ricky-price-89b3b2118/',
+    linkedin: 'www.linkedin.com/in/ricky-price-89b3b2118/',
     twitter: '',
     mastodon: '',
     facebook: '',
